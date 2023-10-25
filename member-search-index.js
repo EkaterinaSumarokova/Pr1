65e6f21fc52a21e48b64c1e62c03a28fb0424a73
Ekaterina <sumarokova.katya@list.ru>
2023-10-25 22:28:06 +0300

    Add Javadoc documentation

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Laba6","l":"Laba6()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Laba6","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
